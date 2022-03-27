9fc6763ef74a19e6ed9ef3bb5105c6a75047b5ac
Victor Manuel <victorvpn13@gmail.com>
2022-03-27 00:48:14 -0300

    feat: Adiciona rota privada para validar o token e redirecionar para a tela de signin caso o token não seja válido

--- src/services/router.tsx
+++ src/services/router.tsx
@@ -1,18 +1,54 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import SocketProvider from '../context/SocketContext';
+import { useUser } from '../context/UserContext';
 
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+const ProtectedRoute = ({
+  children,
+}: {
+  children: any;
+}) => {
+  const { accessToken } = useUser();
+
+  // rota para validar se ele é valido
+  if (!accessToken) {
+    return <Navigate to='/signin' replace />;
+  }
+
+  return children;
+};
+
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<SocketProvider><Home /></SocketProvider>} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
+      <Route
+        path='/'
+        element={
+          <ProtectedRoute>
+            <SocketProvider>
+              <Home />
+            </SocketProvider>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/signin'
+        element={<SignIn />}
+      />
+      <Route
+        path='/signup'
+        element={<SignUp />}
+      />
     </Routes>
   </BrowserRouter>
 );
-  
+
 export default Router;
